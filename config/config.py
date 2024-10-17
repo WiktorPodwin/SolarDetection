d0461269fce19c9709fc62bd5e38fdb3b771a54d
M-Polonczyk <m.polonczyk@outlook.com>
2024-10-17 19:58:22 +0200

    create config with constants

---
+++ config/config.py
@@ -0,0 +1,17 @@
+import os
+import logging
+from attrs import define
+
+logging.basicConfig(level=logging.INFO)
+
+@define
+class BaseConfig:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    GEOPORTAL_URL = "https://polska.geoportal2.pl/map/www/mapa.php?mapa=polska"
+    DATA_DIR = os.path.join(BASE_DIR, "data")
+    IMAGES_DIR = os.path.join(DATA_DIR, "images")
+    LOCATION_FIELD_CSV_DIR = os.path.join(DATA_DIR, "locations.csv")
+
+    BUCKET_NAME = os.getenv("GS_BUCKET_NAME", "solar-panels")
+    SA_CREDENTIALS_PATH = os.path.join(BASE_DIR, "credentials.json")
