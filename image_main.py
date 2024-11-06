89b148fd998f3f37af7aac22e5c43aa1b37ab4c1
WiktorPodwin <272477@student.pwr.edu.pl>
2024-11-06 13:32:23 +0100

    Uprgrade plot detection

--- image_main.py
+++ image_main.py
@@ -1,5 +1,5 @@
-from image_processing.processing_run import process_image
+from processing.processing_run import process_image
 from config.config import BaseConfig as config
 
 if __name__ == "__main__":
-    process_image(config.IMAGES_DIR, "data/images")
+    process_image(config.IMAGES_DIR, "data/cut_out_plots")
--- processing/processing_run.py
+++ processing/processing_run.py
@@ -1,4 +1,4 @@
-from image_processing.processing.image_processing import ImageProcessing
+from processing.image_processing.image_process import ImageProcessing
 from api.src.operations.data_operations import DirectoryOperations
 
 def process_image(input_directory: str, output_directory: str) -> None:
@@ -8,9 +8,8 @@ def process_image(input_directory: str, output_directory: str) -> None:
     for file in files:
         input_path = input_directory + "/" + file
         output_path = output_directory + "/" + file
-        image_processing = ImageProcessing()
 
+        image_processing = ImageProcessing()
         image = image_processing.load_image(input_path)
-        image = image_processing.crop_image(image)
-        image = image_processing.remove_outside_frame(image)
-        image_processing.save_image(output_path, image)
+        cropped_image = image_processing.remove_outside_frame(image)
+        image_processing.save_image(output_path, cropped_image)
