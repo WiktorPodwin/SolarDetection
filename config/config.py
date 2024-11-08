1e6e65a91d358c8f48d0be23ca7726af7bc8e26d
M-Polonczyk <m.polonczyk@outlook.com>
2024-11-08 14:03:59 +0100

    update config

--- config/config.py
+++ config/config.py
@@ -13,5 +13,7 @@ class BaseConfig:
     IMAGES_DIR = os.path.join(DATA_DIR, "images")
     LOCATION_FIELD_CSV_DIR = os.path.join(DATA_DIR, "locations.csv")
 
+    PROJECT_ID = os.getenv("GCP_PROJECT_ID", "solar-panels")
     BUCKET_NAME = os.getenv("GS_BUCKET_NAME", "solar-panels")
     SA_CREDENTIALS_PATH = os.path.join(BASE_DIR, "credentials.json")
+    GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY", "")
