25c47e55ca4007ccdda98b0af13f212fb6792938
M-Polonczyk <m.polonczyk@outlook.com>
2025-01-06 02:04:14 +0100

    x

--- solar_detection/solar_detector/solar_detector.py
+++ solar_detection/solar_detector/solar_detector.py
@@ -19,7 +19,7 @@ class SolarRoofDetector(nn.Module):
         self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm2d(64)
-        
+
         self.conv4 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.bn4 = nn.BatchNorm2d(32)
         self.conv5 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
